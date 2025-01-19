08c65724df6331ff2c692263ad30dd0dcdf5688c
LetsGoWorld <iambearbg@gmail.com>
2025-01-18 23:29:51 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -10,15 +10,15 @@ export async function before(m, {conn, participants, groupMetadata}) {
   if (chat.bienvenida && m.messageStubType == 27) {
     let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 
-await conn.sendAi(m.chat, botname, textbot, bye, img, img)
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, rcanal)
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
     let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Nunca te quisimos   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img)
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, rcanal)
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
     let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Nunca te quisimos   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img)
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, rcanal)
 }}
