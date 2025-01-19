1df5ef502d7172bb17ca188c9f78c92b61e621a4
LetsGoWorld <iambearbg@gmail.com>
2025-01-19 10:01:16 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,22 +3,22 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/ylgu47w3')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/p0JpJ6G/file.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
     let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, rcanal)
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
     let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Nunca te quisimos   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, rcanal)
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
     let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Nunca te quisimos   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, rcanal)
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
 }}
