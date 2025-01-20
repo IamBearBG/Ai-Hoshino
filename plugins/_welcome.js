4326604cf34ce3c5e889ad83d02fdc29f160b80d
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-01-19 19:41:18 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -9,8 +9,23 @@ export async function before(m, {conn, participants, groupMetadata}) {
 
   if (chat.bienvenida && m.messageStubType == 27) {
     let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
-
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
+conn.sendMessage(m.chat, { text: bienvenida },
+forwardedNewsletterMessageInfo: { 
+  newsletterName: 'NOMBRE DEL CANAL', 
+  newsletterJid: 'ID DEL CANAL'
+}, 
+contextInfo: {
+mentionedJid: conn.parseMention(bienvenida),
+isForwarded: true,
+forwardingScore: 999,
+externalAdReply: {
+title: botname,
+body: textbot,
+thumbnail: img,
+sourceUrl: 'no se w',
+mediaType: 1,
+renderLargerThumbnail: true
+}}, { quoted: m })
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
